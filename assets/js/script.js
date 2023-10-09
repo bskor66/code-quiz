74c60c31ddb168b081302cc04680a3e79e984243
Blake Skor <blake.skor@outlook.com>
2023-10-09 16:44:55 -0400

    add questions template + array in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,31 @@ let startGameSection = document.getElementById("start-game-section");
 
 //Question Screen Elements
 let questionSection = document.getElementById("question-section");
+let questionHeader = questionSection.querySelector("h2");
+let optionsOL = questionSection.querySelector("ol");
+
+//Questions
+const questionTemplate = {
+	question: "",
+	choices: [],
+	correctAnswer: "",
+};
+
+const questions = [
+	{
+		//inherit template structure
+		...questionTemplate,
+		question: "What is the capital of France?",
+		choices: ["London", "Berlin", "Paris", "Madrid"],
+		correctAnswer: "Paris",
+	},
+	{
+		...questionTemplate,
+		question: "Which planet is known as the Red Planet?",
+		choices: ["Mars", "Venus", "Jupiter", "Earth"],
+		correctAnswer: "Mars",
+	},
+];
 
 //Begin Game
 function beginGame() {
