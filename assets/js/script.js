f1256b074408ebc2a824bcbf0135df50a6fa6319
Blake Skor <blake.skor@outlook.com>
2023-10-09 16:33:51 -0400

    Add question screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
+//Start Game Elements
 let startGameButton = document.getElementById("start-game-button");
 let startGameSection = document.getElementById("start-game-section");
 
+//Question Screen Elements
+let questionSection = document.getElementById("question-section");
+
+//Begin Game
 function beginGame() {
 	startGameSection.style.display = "none";
+	questionSection.style.display = "flex";
 }
 
+//Start Game Button on Click
 startGameButton.addEventListener("click", beginGame);
